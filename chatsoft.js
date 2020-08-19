770e000161f8e9854e2540bdc01b32a321057596
MoPaMo <mpm1301@web.de>
2020-08-19 20:29:17 +0200

    added class

--- chatsoft.js
+++ chatsoft.js
@@ -1,35 +1,41 @@
-function fetchJSONFile(path, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
+class chatsoft {
+
+    fetchJSONFile(path, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if (callback) callback(data);
+                }
             }
-        }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send();
-}
+        };
+        httpRequest.open('GET', path);
+        httpRequest.send();
+    }
+    constructor(path) {
+        this.text = "";
+        this.reg = "";
+        this.match = "";
+        this.JSONanswer = fetchJSONFile('aw.json', function (data) { return data })
 
-// this requests the file and executes a callback with the parsed result once
-//   it is available
-var text, reg, match
-function test() {
-    text = document.getElementById("demo").value; fetchJSONFile('aw.json', function (data) {
-        data.forEach(function (item, index) {
-            reg = new RegExp(item.q)
-            if (reg.test(text)) {
+    }
+    answer() {
+        this.text = document.getElementById("demo").value;
+        this.JSONanswer.forEach(function (item, index) {
+            this.reg = new RegExp(item.q)
+            if (this.reg.test(this.text)) {
 
-                match = text.match(reg)
+                this.match = this.text.match(reg)
                 var F = new Function(item.a);
                 return (F());
             }
             else { }
 
-        });return("Not found")
-    
+        }); return ("Not found")
 
 
-    });
+
+
+    }
 }
