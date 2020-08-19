e97a5475cb296233c517bd2c5ec01a0de9d2616d
MoPaMo <mpm1301@web.de>
2020-08-19 16:22:43 +0200

    formatted all

--- chatsoft.js
+++ chatsoft.js
@@ -1,4 +1,6 @@
-function test(){text=document.getElementById("demo").value
-var match = text.match(/search for (\w+)( |)/i) 
-if(/search for (\w+)( |)/i.test(text))
-open("https://wikipedia.org/wiki/"+match[1],"wiki");}
+function test() {
+    text = document.getElementById("demo").value
+    var match = text.match(/search for (\w+)( |)/i)
+    if (/search for (\w+)( |)/i.test(text))
+        open("https://wikipedia.org/wiki/" + match[1], "wiki");
+}
