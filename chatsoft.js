bb97ee16ba05eb6362af4109a537820151fef0e0
MoPaMo <mpm1301@web.de>
2020-08-19 16:21:03 +0200

    REGEX WORKINIG NOW

--- chatsoft.js
+++ chatsoft.js
@@ -1,15 +1,4 @@
-const regex = /^search for ([\w])+$/g
-const str = `search for h`;
-let m;
-
-while ((m = regex.exec(str)) !== null) {
-    // This is necessary to avoid infinite loops with zero-width matches
-    if (m.index === regex.lastIndex) {
-        regex.lastIndex++;
-    }
-    
-    // The result can be accessed through the `m`-variable.
-    m.forEach((match, groupIndex) => {
-        open("https://wikipedia.com/wiki/"+match)
-    });
-}
+function test(){text=document.getElementById("demo").value
+var match = text.match(/search for (\w+)( |)/i) 
+if(/search for (\w+)( |)/i.test(text))
+open("https://wikipedia.org/wiki/"+match[1],"wiki");}
