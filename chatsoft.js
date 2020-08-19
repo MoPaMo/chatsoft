9b48eb6b5aa52a711479228eaf413fad155e25c4
MoPaMo <mpm1301@web.de>
2020-08-19 17:06:29 +0200

    added ajax

--- chatsoft.js
+++ chatsoft.js
@@ -1,7 +1,31 @@
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
+
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+
 function test() {
-    let text = document.getElementById("demo").value
-    let reg = new RegExp(/search for (\w+)( |)/i)
-    var match = reg.test(text)
-    if (reg.test(text))
-        open("https://wikipedia.org/wiki/" + match[0], "wiki");
+    let text = document.getElementById("demo").value; fetchJSONFile('aw.json', function (data) {
+        data.forEach(function (item, index) {
+            let reg = new RegExp(/^search for (\w+)( |)$/i)
+            console.log(item.q)
+            //if()    
+        });
+
+
+        var match = text.match(reg)
+        if (reg.test(text))
+            open("https://wikipedia.org/wiki/" + match[1], "wiki");
+    });
 }
